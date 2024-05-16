c8a9d5155ff4e7a34eb876dc3792263f90e5b15b
Bill Thell <bill.thell@sdnit.se>
2024-05-16 23:52:19 +0200

    tonights lessons

--- frontent/01-starting-project/src/App.jsx
+++ frontent/01-starting-project/src/App.jsx
@@ -1,14 +1,33 @@
-import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react';
+
+import { EXAMPLES , CORE_CONCEPTS} from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+    const [ selectedTopic, setSelectedTopic ]  = useState();
+
+
     function handleSelect(selectedButton) {
-        console.log(selectedButton);
+        setSelectedTopic(selectedButton);
     }
 
-    const buttons = ["Component", "JSX", "Props", "State"];
+    const buttons = ["components", "jsx", "props", "State"];
+    let tab_content = <p>Please select a topic.</p>
+
+    if (selectedTopic) {
+        tab_content = (
+        <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+                <code>
+                    {EXAMPLES[selectedTopic].code}
+                </code>
+            </pre>
+        </div> );
+    }
 
   return (
     <div>
@@ -17,18 +36,17 @@ function App() {
       <section id="core-concepts">
       <h2>Core Concepts</h2>
       <ul>
-        {CORE_CONCEPTS.map(CoreConcept)}
+        {CORE_CONCEPTS.map((i) => (
+            <CoreConcept key={i.title} {...i} />
+        ))}
       </ul>
       </section>
       <section id="examples">
         <h2>Examples</h2>
         <menu>
-           {buttons.map((x) => <TabButton onClick={() => handleSelect(x.toLowerCase())}>{x}</TabButton>)}
-           <TabButton onClick={() => handleSelect('jsx')}>JSX</TabButton>
-           <TabButton onClick={() => handleSelect('props')}>Props</TabButton>
-           <TabButton onClick={() => handleSelect('state')}>Sate</TabButton>
+           {buttons.map((x) => <TabButton key={x.toLowerCase()} isSelected={selectedTopic === x} onClick={() => handleSelect(x.toLowerCase())}>{x}</TabButton>)}
         </menu>
-          Dynamic Content   
+      {tab_content}
       </section>
       </main>
     </div>
--- frontent/01-starting-project/src/components/TabButton.jsx
+++ frontent/01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({children, onClick}) {
+export default function TabButton({children, onClick, isSelected}) {
     return (
         <li>
-            <button onClick={onClick}>{children}</button>
+            <button className={isSelected ? "active" : ""} onClick={onClick}>{children}</button>
         </li>
     );
 }
