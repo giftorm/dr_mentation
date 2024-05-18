97e769a998e41c706478dfc3f1b9a743d46086eb
Bill Thell <bill.thell@sdnit.se>
2024-05-18 22:28:25 +0200

    remove files from course

--- frontent/01-starting-project/src/components/TabButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function TabButton({children, onClick, isSelected}) {
-    return (
-        <li>
-            <button className={isSelected ? "active" : ""} onClick={onClick}>{children}</button>
-        </li>
-    );
-}
