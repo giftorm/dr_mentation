626dda4e6b99b780a5b2ebc1e17f65397e451792
giftorm <arganka@live.se>
2024-05-22 11:01:38 +0200

    Cors and http requests from react (#15)
    feat: CORS Settings on the api, began http client in react

--- frontent/DrMentation/src/App.jsx
+++ frontent/DrMentation/src/App.jsx
@@ -1,38 +1,53 @@
 import { useState, React, Fragment } from 'react';
+
+import { GetDocument, PostDocument } from './client/document';
+
+import { Document } from './model/Document';
+
 import Header from './components/Header';
-import Markdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialOceanic } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import remarkGfm from 'remark-gfm';
-import rehypeSanitize from 'rehype-sanitize';
-import rehypeExternalLinks from 'rehype-external-links';
+import MarkdownRenderer from './components/MarkdownRenderer';
+import Editor from './components/Editor';
+import SubHeader from './components/SubHeader';
+
 
 function App() {
   const [editMode, setEditMode] = useState(false);
   const [source, setSource] = useState('');
-  const options = { code: CodeBlock, pre: Pre };
+  const [currentDocument, setCurrentDocument] = useState(undefined);
 
-  const handleSave = () => {
+  function handleSave() {
     console.log("saved");
     setEditMode(false);
   };
 
-  const handleExit = () => {
+  function handleExit() {
     setEditMode(false);
   };
 
-  const handleNew = () => {
-    setSource('');
+  function handleNew() {
+    let newDoc = Document;
+    console.log(newDoc);
+    setCurrentDocument(newDoc);
     setEditMode(true);
   };
 
-  const handleEdit = () => {
+  function handleEdit() {
     setEditMode(true);
   };
 
-  const feedElement = (syntax) => {
-    setSource((prevSource) => prevSource + syntax);
+  function feedElement(syntax) {
+      setCurrentDocument((prevDocument) => ({
+        ...prevDocument,
+        content: prevDocument.content + syntax,
+      }));
   };
+
+ function updateDocument(content) {
+    setCurrentDocument((prevDocument) => ({
+      ...prevDocument,
+      content: content,
+    }));
+  }
 
   return (
     <Fragment>
@@ -43,113 +58,22 @@ function App() {
           onSave={handleSave}
           onExit={handleExit}
           onNew={handleNew}
+          onGet={GetDocument}
+          onPost={PostDocument}
           onEdit={handleEdit}
           feedElement={feedElement}
         />
         <div className='flex-grow flex justify-center'>
           <div className={`flex ${editMode ? 'flex-grow' : 'justify-center'} max-w-[1794px] w-full`}>
-            {editMode && <Editor source={source} onChange={setSource} />}
+            {editMode && <Editor content={currentDocument.content} onChange={updateDocument} />}
             {editMode && (
               <div className='w-[2px] border-l-2 border-text border-dashed'></div>
             )}
-            <MarkdownArea source={source} options={options} />
+            <MarkdownRenderer document={currentDocument} />
           </div>
         </div>
       </div>
     </Fragment>
-  );
-}
-
-function Editor({ source, onChange }) {
-  return (
-    <section className='flex-grow w-full pt-5 max-w-4xl'>
-      <textarea
-        className='p-5 text-text w-full bg-transparent h-full resize-none focus:outline-none placeholder:text-lg placeholder:text-white placeholder:tracking-wider placeholder:opacity-80'
-        placeholder='Markdown goes here...'
-        onChange={(e) => onChange(e.target.value)}
-        value={source}
-        autoFocus
-      />
-    </section>
-  );
-}
-
-function MarkdownArea({ source, options }) {
-  return (
-    <article className='flex-grow w-full p-5 overflow-auto max-w-4xl'>
-      <Markdown
-        className='prose prose-invert break-words max-w-full'
-        components={options}
-        remarkPlugins={[remarkGfm]}
-        rehypePlugins={[
-          rehypeSanitize,
-          [rehypeExternalLinks, { content: { type: 'text', value: '🔗' } }],
-        ]}
-      >
-        {source}
-      </Markdown>
-    </article>
-  );
-}
-
-function CodeBlock({ ...props }) {
-  return (
-    <SyntaxHighlighter
-      language={props.className?.replace(/(?:lang(?:uage)?-)/, '')}
-      style={materialOceanic}
-      wrapLines={true}
-      className='not-prose rounded-md'
-    >
-      {props.children}
-    </SyntaxHighlighter>
-  );
-}
-
-function Pre({ ...props }) {
-  return <div className='not-prose'>{props.children}</div>;
-}
-
-function SubHeader({ editMode, onSave, onExit, onNew, onEdit, feedElement }) {
-  const buttonStyle = 'flex text-xl px-4 py-2 text-text rounded-md font-primary hover:bg-gray-700';
-
-  if (editMode) {
-    const btns = [
-      { name: 'B', syntax: '**Bold**' },
-      { name: 'I', syntax: '*Italic*' },
-      { name: 'S', syntax: '~Strikethrough~' },
-      { name: 'H1', syntax: '# ' },
-    ];
-
-    return (
-      <header className='flex items-center h-14 sticky border-t-2 justify-center'>
-        <div className='flex space-x-4'>
-          {btns.map((btn) => (
-            <button key={btn.syntax} className={buttonStyle} onClick={() => feedElement(btn.syntax)}>
-              {btn.name}
-            </button>
-          ))}
-          <button key='save' className={buttonStyle} onClick={onSave}>
-            save
-          </button>
-          <button key='exit' className={buttonStyle} onClick={onExit}>
-            exit
-          </button>
-        </div>
-      </header>
-    );
-  }
-
-  return (
-    <header className='flex items-center h-14 sticky border-t-2 justify-center'>
-      <div className='flex space-x-4'>
-        <button key='new' className={buttonStyle} onClick={onNew}>
-          new
-        </button>
-        <button key='edit' className={buttonStyle} onClick={onEdit}>
-          edit
-        </button>
-      </div>
-    </header>
   );
 }
 
